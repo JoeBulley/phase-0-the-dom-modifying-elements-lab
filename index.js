d7da39b388dad0740968dba867167e7aa8b37f3c
JoeBulley <bulleyyy@gmail.com>
2021-11-22 15:50:51 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+//class or id doesnt need to be defined with # or . like in css
+// create elements that arent there before defining them
